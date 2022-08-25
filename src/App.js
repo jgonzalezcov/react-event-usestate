4ae99a72336f1b0361440e1233386524b36fc138
Juan Gonzalez Covarrubias <104181238+jgonzalezcov@users.noreply.github.com>
2022-08-25 10:48:11 -0400

    feat: First working version of the code is created.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
+import { useState } from 'react'
+import Button from './components/Button'
+import Header from './components/Header'
+import Input from './components/Input'
+
 function App() {
-  return <div className="App"></div>
+  const buttonClean = () => {
+    setPassw('')
+    setName('')
+    if (inName === '') {
+      alert('Debes ingresar tu nombre')
+    }
+  }
+  const [inPassw, setPassw] = useState('')
+  const [inName, setName] = useState('')
+  return (
+    <div className="App">
+      <Header />
+      <Input
+        setPassw={setPassw}
+        setName={setName}
+        inName={inName}
+        inPassw={inPassw}
+      />
+      {inPassw === '252525' ? <Button buttonClean={buttonClean} /> : ''}
+    </div>
+  )
 }
 
 export default App
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
 
-const Button = () => {
-  return <div>Button</div>
+const Button = (props) => {
+  return (
+    <div className="container-button">
+      <button
+        onClick={() => {
+          props.buttonClean()
+        }}
+      >
+        Ingresar
+      </button>
+    </div>
+  )
 }
 
 export default Button
